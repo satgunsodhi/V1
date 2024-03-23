3c3152b227544ff00bc62654c9eba3eafb3bac2c
vansh-chani <162613758+vansh-chani@users.noreply.github.com>
2024-03-23 12:08:38 +0530

    adding FindRoute and FindCost

--- master.cpp
+++ master.cpp
@@ -2,13 +2,21 @@
 
 class master {
     protected:
-    char ClassName[255];
+    char DepartingLocation[255];
     char Destination[255];
-    char route1[255];
-    char route2[255];
     float HandlingCharges;
     float ServiceFees;
     float GST;
+
+    void FindRoute()
+    {
+        // route finding
+    };
+
+    float FindCost()
+    {
+        // cost finding
+    };
 };
 
 class Time {
