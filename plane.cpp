71d036f327219742381c7e6e12336d4341b0a3e5
vansh-chani <162613758+vansh-chani@users.noreply.github.com>
2024-03-29 00:23:54 +0530

    defining plane structure

--- plane.cpp
+++ plane.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 #include "master.cpp"
 using namespace std;
 
 class plane: private master {
-    //hello
+    int airline;
+    int noOfAdults;
+    int noOfMinors;
+    int routeOption;
+    int seatType;
+    int stops;
+    
+    public:
+    plane(int noOfAdults, int noOfMinors, int routeOption , int seatType, int stops ,char[] DepartingLocation, char[] Destination)
+    {
+        this -> noOfAdults = noOfAdults;
+        this -> noOfMinors = noOfMinors;
+        this -> routeOption = routeOption;
+        this -> seatType = seatType;
+        this -> stops = stops;
+        strcpy(this -> DepartingLocation,DepartingLocation);
+        strcpy(this -> Destination,Destination);
+    }
+
+    char* oneWayRoutes();
+    char* roundTripRoutes();
+    char* MultiCityRoutes();
+
 };
+
+void showPlane()
+{
+    // display all the available routes
+}
+
+
+void AddPlane(char DepartingLocation[], char Destination[]) 
+{
+    int noOfAdults;
+    int noOfMinors;
+    int routeOption;
+    int seatType;
+    int stops;
+    cin >> noOfAdults;
+    cin >> noOfMinors;
+    cin >> routeOption;
+    cin >> seatType;
+    cin >> stops;
+    plane p1(noOfAdults,noOfMinors,routeOption,seatType,stops,DepartingLocation,Destination);
+    showPlane();
+}
