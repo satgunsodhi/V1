c80959d8e09ae62fd01118f640d2a94e55547bbd
satgunsodhi <146955596+satgunsodhi@users.noreply.github.com>
2024-03-29 22:37:49 +0530

    update 0.3

--- main.cpp
+++ main.cpp
@@ -9,9 +9,31 @@
 using namespace std;
 
 int main() {
-    cout >> "Enter Personal Details:";
-    char NameofPassengers[];
-    int NoofPassengers[];
-    cin << NameofPassengers;
-    cin << NoofPassengers;
+    cout << "Enter Personal Details:\nEnter the no of passengers:";
+    int choice;
+    int NoOfPassengers;
+    char DepartingLocation[255];
+    char Destination[255];
+    cin >> NoofPassengers;
+    cout << "Enter the name of passengers";
+    char NameofPassengers[NoOfPassengers];
+    for(int i = 0; i < NoOfPassengers; i++) {
+        cin >> NameOfPassengers[i];
+    }
+    cout << "What brings you today?:\n1. Book a Plane\n2. Book a Train\n3. Rent a Car\n4. Stay at a hotel";
+    cin >> choice;
+    cout << "Enter your Departing Location";
+    cin >> DepartingLocation;
+    cout << "Enter your Destination";
+    cin >> Destination;
+    switch(choice) {
+        case 1:
+            Addplane(NoOfPassengers, NameOfPassengers, DepartingLocation, Destination);
+        case 2:
+            Addtrain(NoOfPassengers, NameOfPassengers, DepartingLocation, Destination);
+        case 3:
+            RentCar(NoOfPassengers, NameOfPassengers, DepartingLocation, Destination);
+        case 4:
+            AddHotel(NoOfPassengers, NameOfPassengers, DepartingLocation, Destination);
+    }
 }
