01c5582110181c9d748643543cb46446ae253e80
anwesha-cloud <62639539+anwesha-cloud@users.noreply.github.com>
2024-04-12 19:43:14 +0530

    car.cpp

--- car.cpp
+++ car.cpp
@@ -1,55 +1,60 @@
+//#include "master.cpp"
 #include <iostream>
-#include "master.cpp"
 #include <string>
-#include <cstring>
+#include <iomanip> // for std::endl
 using namespace std;
 
 class Car {
-    public:
+public:
     string location;
-    char choice2[2];
+    string choice2;
     int distance;
     int distanceCost;
     int seatType;
     Car() : distance(0), distanceCost(0) {}
 
     void calculateFare() {
-        cin>>location;
-        if(location == "A"){
-            cout<<"A1"<<endl<<"Do u want to go to A1?[Y/N]";
-            cin>>choice2;
-            if(choice2[0] == 'Y'){
-                distance+=20;
+        cout << "Enter your location (A/B/C): ";
+        cin >> location;
+        if (location == "A") {
+            cout << "A1\nDo you want to go to A1? [Y/N]: ";
+            cin >> choice2;
+            if (choice2[0] == 'Y' || choice2[0] == 'y') {
+                distance += 25;
             }
-            cout<<"A2"<<endl<<"Do u want to go to A2?[Y/N]";
-            cin>>choice2;
-            if(choice2[0] == 'Y'){
-                distance+=20;
+            cout << "A2\nDo you want to go to A2? [Y/N]: ";
+            cin >> choice2;
+            if (choice2[0] == 'Y' || choice2[0] == 'y') {
+                distance += 25;
             }
-            cout<<"A3"<<endl<<"Do u want to go to A3?[Y/N]";
-            cin>>choice2;
-            if(choice2[0] == 'Y'){
-                distance+=20;
+            cout << "A3\nDo you want to go to A3? [Y/N]: ";
+            cin >> choice2;
+            if (choice2[0] == 'Y' || choice2[0] == 'y') {
+                distance += 25;
             }
-            cout<<"A4"<<endl<<"Do u want to go to A4?[Y/N]";
-            cin>>choice2;
-            if(choice2[0] == 'Y'){
-                distance+=20;
+            cout << "A4\nDo you want to go to A4? [Y/N]: ";
+            cin >> choice2;
+            if (choice2[0] == 'Y' || choice2[0] == 'y') {
+                distance += 25;
             }
-            cout<<"A5"<<endl<<"Do u want to go to A5?[Y/N]";
-            cin>>choice2;
-            if(choice2[0] == 'Y'){
-                distance+=20;
+            cout << "A5\nDo you want to go to A5? [Y/N]: ";
+            cin >> choice2;
+            if (choice2[0] == 'Y' || choice2[0] == 'y') {
+                distance += 25;
             }
-            cin>>"Car Type[5 seater/7 seater]">>seatType;
-            if(seatType==5){
-                 distanceCost=distance*20;
-                 cout<<"Total Fare"<<distanceCost;
+            cout << "What car type will you prefer[5 seater/7 seater]: ";
+            cin >> seatType;
+            if (seatType == 5) {
+                distanceCost = distance * 19.5;
+                cout << "Total Fare: " << distanceCost << endl;
             }
-            else if(seatType==7){
-                distanceCost=distance*30;
-                cout<<"Total Fare"<<distanceCost;
+            else if (seatType == 7) {
+                distanceCost = distance * 33;
+                cout << "Total Fare: " << distanceCost << endl;
+            }
+            else {
+                cout << "Please enter either 5 or 7 for car type." << endl;
             }
         }
-
+    }
 };
