fa119af9f3157687ff16d95ff16d5ede3a7835a7
Akriti Agarwal <akritiagarwal05@gmail.com>
2024-04-23 20:30:00 +0530

    feat:task

--- hotel.cpp
+++ hotel.cpp
@@ -8,19 +8,19 @@ using namespace std;
 class Hotel: public master
 {
     public:
-    int location;
-    int num;
-    Time duration;
     int roomcount;
     float price;
     char hotelName[40];
     char roomType[20];
     void display();
-    Hotel(Hotel &h1)
+    void f1(const char * roomname, int people, int base);
+    Hotel(master &m) : master("Ignore")
     {
-        location = Destination.LocationId;
-        num = NoofPassengers;
-        duration = Duration;
+        this->Destination.LocationId = m.Destination.LocationId;
+        this-> NoofPassengers = m.NoofPassengers;
+        this->Duration = m.Duration;
+        GST=12;
+        display();
     }
 };
 
@@ -28,32 +28,31 @@ void Hotel::display()
 {
     int c1,c2;
 
-    //LOCATION 1-MANALI
-    if(location==1)
+    //Destination.LocationId 1-MANALI
+    if(Destination.LocationId==1)
     {
         while(true)
         {
-            cout<<"MAIN MENU:"<<endl<<"1. The Royal Majestic Mountain View"<<endl<<"2. Hotel New Meadows Inn"<<endl<<"3. Exit";
-            cout<<"Enter your choice: ";
+            cout<<"\nMAIN MENU:"<<endl<<"1. The Royal Majestic Mountain View"<<endl<<"2. Hotel New Meadows Inn"<<endl<<"3. Exit";
+            cout<<"\nEnter your choice: ";
             cin>>c1;
             if(c1==1)
             {
                 strcpy(hotelName,"The Royal Majestic Mountain View");
-                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
-                cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
+                cout<<"\nMENU:"<<endl<<"1. King Room (2 guests max,4000/-)"<<endl<<"2. Deluxe Family Suite (4 guests max,7000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"King Room");
-                    roomcount=ceil(num/2);
-                    price=(4000-roomcount)+(num-roomcount)*300;
+                    f1("King Room", 2,4000);
+                    break;
                 }
+
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Deluxe Family Suite");
-                    roomcount=ceil(num/4);
-                    price=(7000-roomcount)+(num-roomcount)*300;
+                    f1("Deluxe Family Suite", 4,7000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -63,21 +62,19 @@ void Hotel::display()
             else if(c1==2)
             {
                 strcpy(hotelName,"Hotel New Meadows Inn");
-                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
-                cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna\n";
+                cout<<"\nMENU:"<<endl<<"1. Double Room (2 guests max,2000/-)"<<endl<<"2. Family Room with Balcony (4 guests max,5000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"Double Room");
-                    roomcount=ceil(num/2);
-                    price=(2000-roomcount)+(num-roomcount)*300;
+                    f1("Double Room", 2,2000);
+                    break;
                 }
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Family Room with Balcony");
-                    roomcount=ceil(num/4);
-                    price=(5000-roomcount)+(num-roomcount)*300;
+                    f1("Family Room with Balcony", 4,5000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -91,32 +88,30 @@ void Hotel::display()
         }
     }
 
-    //LOCATION 2-GOA
-    else if(location==2)
+    //Destination.LocationId 2-GOA
+    else if(Destination.LocationId==2)
     {
         while(true)
         {
-            cout<<"MAIN MENU:"<<endl<<"1. Casa De Goa"<<endl<<"2. The Fern Habitat"<<endl<<"3. Exit";
-            cout<<"Enter your choice: ";
+            cout<<"\nMAIN MENU:"<<endl<<"1. Casa De Goa"<<endl<<"2. The Fern Habitat"<<endl<<"3. Exit";
+            cout<<"\nEnter your choice: ";
             cin>>c1;
             if(c1==1)
             {
                 strcpy(hotelName,"Casa De Goa");
-                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
-                cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
+                cout<<"\nMENU:"<<endl<<"1. King Room (2 guests max,1000/-)"<<endl<<"2. Deluxe Family Suite (4 guests max,4000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"King Room");
-                    roomcount=ceil(num/2);
-                    price=(1000-roomcount)+(num-roomcount)*300;
+                    f1("King Room", 2,1000);
+                    break;
                 }
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Deluxe Family Suite");
-                    roomcount=ceil(num/4);
-                    price=(4000-roomcount)+(num-roomcount)*300;
+                    f1("Deluxe Family Suite", 4,4000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -126,21 +121,19 @@ void Hotel::display()
             else if(c1==2)
             {
                 strcpy(hotelName,"The Fern Habitat");
-                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
-                cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
+                cout<<"\nMENU:"<<endl<<"1. Double Room (2 guests max,3000/-)"<<endl<<"2. Family Room with Balcony (4 guests max,6000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"Double Room");
-                    roomcount=ceil(num/2);
-                    price=(3000-roomcount)+(num-roomcount)*300;
+                    f1("Double Room", 2,3000);
+                    break;
                 }
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Family Room with Balcony");
-                    roomcount=ceil(num/4);
-                    price=(6000-roomcount)+(num-roomcount)*300;
+                    f1("Family Room with Balcony", 4,6000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -154,32 +147,30 @@ void Hotel::display()
         }
     }
 
-    //LOCATION 3- SIKKIM
-    else if(location==3)
+    //Destination.LocationId 3- SIKKIM
+    else if(Destination.LocationId==3)
     {
         while(true)
         {
-            cout<<"MAIN MENU:"<<endl<<"1. Golden Crescent"<<endl<<"2. Vivanta"<<endl<<"3. Exit";
-            cout<<"Enter your choice: ";
+            cout<<"\nMAIN MENU:"<<endl<<"1. Golden Crescent"<<endl<<"2. Vivanta"<<endl<<"3. Exit";
+            cout<<"\nEnter your choice: ";
             cin>>c1;
             if(c1==1)
             {
                 strcpy(hotelName,"Golden Crescent");
-                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
-                cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
+                cout<<"\nMENU:"<<endl<<"1. King Room (2 guests max, 2000/-)"<<endl<<"2. Deluxe Family Suite (4 guests max, 5000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"King Room");
-                    roomcount=ceil(num/2);
-                    price=(2000-roomcount)+(num-roomcount)*300;
+                    f1("King Room", 2,2000);
+                    break;
                 }
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Deluxe Family Suite");
-                    roomcount=ceil(num/4);
-                    price=(5000-roomcount)+(num-roomcount)*300;
+                    f1("Deluxe Family Suite", 4,5000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -189,21 +180,19 @@ void Hotel::display()
             else if(c1==2)
             {
                 strcpy(hotelName,"Vivanta");
-                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
-                cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
-                cout<<"Choose room type: ";
+                cout<<"\nFACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
+                cout<<"\nMENU:"<<endl<<"1. Double Room (2 guests max, 5000/-)"<<endl<<"2. Family Room with Balcony (4 guests max, 10000/-)"<<endl<<"3. Go back to MAIN";
+                cout<<"\nChoose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
-                    strcpy(roomType,"Double Room");
-                    roomcount=ceil(num/2);
-                    price=(5000-roomcount)+(num-roomcount)*300;
+                    f1("Double Room", 2,5000);
+                    break;
                 }
                 else if(c2==2)
                 {
-                    strcpy(roomType,"Family Room with Balcony");
-                    roomcount=ceil(num/4);
-                    price=(10000-roomcount)+(num-roomcount)*300;
+                    f1("Family Room with Balcony", 4,10000);
+                    break;
                 }
                 else if(c2==3)
                 {
@@ -216,5 +205,13 @@ void Hotel::display()
             }
         }
     }
-    //price=price*duration;
+    price=price*ceil(Duration.timeReadOut()/24);
+}
+void Hotel::f1(const char * roomname, int people, int base) 
+{
+    strcpy(roomType, roomname);
+    roomcount=ceil(NoofPassengers/people);
+    price=((base-roomcount)+(NoofPassengers-roomcount)*300)*(1+GST/100);
+    cout<<"\nPrice for Hotel: "<<price<<"\\-";
+    cout<<"\nHotel Booked!!" << endl;
 }
