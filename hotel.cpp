63fc3828fc7f693c94fa4ea6a74621cce2dc5061
Akriti Agarwal <akritiagarwal05@gmail.com>
2024-04-23 17:17:09 +0530

    feat:hotel

--- hotel.cpp
+++ hotel.cpp
@@ -1,23 +1,27 @@
 #include "master.cpp"
 #include "main.cpp"
 #include <iostream>
-#include <cmath>
+#include <ctype.h>
+#include <string.h>
 using namespace std;
 
 class Hotel
 {
+    public:
     int location;
     int num;
+    int duration;
     int roomcount;
     float price;
     char hotelName[40];
-    public:
-        void display();
-        hotel(int location,int noOfpeople)
-        {
-            this->location=location;
-            this->num=noOfpeople;
-        }
+    char roomType[20];
+    void display();
+    Hotel(int location,int noOfpeople,int duration)
+    {
+        this->location=location;
+        this->num=noOfpeople;
+        this->duration=duration;
+    }
 };
 
 void Hotel::display()
@@ -34,17 +38,20 @@ void Hotel::display()
             cin>>c1;
             if(c1==1)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"The Royal Majestic Mountain View");
+                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
                 cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"King Room");
                     roomcount=ceil(num/2);
                     price=(4000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Deluxe Family Suite");
                     roomcount=ceil(num/4);
                     price=(7000-roomcount)+(num-roomcount)*300;
                 }
@@ -55,17 +62,20 @@ void Hotel::display()
             }
             else if(c1==2)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"Hotel New Meadows Inn");
+                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
                 cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"Double Room");
                     roomcount=ceil(num/2);
                     price=(2000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Family Room with Balcony");
                     roomcount=ceil(num/4);
                     price=(5000-roomcount)+(num-roomcount)*300;
                 }
@@ -91,17 +101,20 @@ void Hotel::display()
             cin>>c1;
             if(c1==1)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"Casa De Goa");
+                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
                 cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"King Room");
                     roomcount=ceil(num/2);
                     price=(1000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Deluxe Family Suite");
                     roomcount=ceil(num/4);
                     price=(4000-roomcount)+(num-roomcount)*300;
                 }
@@ -112,17 +125,20 @@ void Hotel::display()
             }
             else if(c1==2)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"The Fern Habitat");
+                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
                 cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"Double Room");
                     roomcount=ceil(num/2);
                     price=(3000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Family Room with Balcony");
                     roomcount=ceil(num/4);
                     price=(6000-roomcount)+(num-roomcount)*300;
                 }
@@ -148,17 +164,20 @@ void Hotel::display()
             cin>>c1;
             if(c1==1)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"Golden Crescent");
+                cout<<"FACILITIES AVAILABLE: Complementary Breakfast, Free Parking and WIFI, Pool and Gym inclusive";
                 cout<<"MENU:"<<endl<<"1. King Room (2 guests max)"<<endl<<"2. Deluxe Family Suite (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"King Room");
                     roomcount=ceil(num/2);
                     price=(2000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Deluxe Family Suite");
                     roomcount=ceil(num/4);
                     price=(5000-roomcount)+(num-roomcount)*300;
                 }
@@ -169,17 +188,20 @@ void Hotel::display()
             }
             else if(c1==2)
             {
-                cout<<"FACILITIES AVAILABLE: ";
+                strcpy(hotelName,"Vivanta");
+                cout<<"FACILITIES AVAILABLE: Complementary Brekafast, Free WIFI, Spa and Sauna";
                 cout<<"MENU:"<<endl<<"1. Double Room (2 guests max)"<<endl<<"2. Family Room with Balcony (4 guests max)"<<endl<<"3. Go back to MAIN";
                 cout<<"Choose room type: ";
                 cin>>c2;
                 if(c2==1)
                 {
+                    strcpy(roomType,"Double Room");
                     roomcount=ceil(num/2);
                     price=(5000-roomcount)+(num-roomcount)*300;
                 }
                 else if(c2==2)
                 {
+                    strcpy(roomType,"Family Room with Balcony");
                     roomcount=ceil(num/4);
                     price=(10000-roomcount)+(num-roomcount)*300;
                 }
@@ -194,4 +216,5 @@ void Hotel::display()
             }
         }
     }
+    price=price*duration;
 }
